4a66a6180cdbed028fe157410c217c976b313d30
zburu <shuxhan@163.com>
2022-10-20 14:38:44 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Tampermonkey Nmae
+// @namespace    https://github.com/zburu
+// @version      0.0.1
+// @description  Tampermonkey description
+// @author       zburu
+// @match        http://*/*
+// @match        https://*/*
+// @icon         https://www.google.com/chrome/static/images/chrome-logo-m100.svg
+// @require      https://cdn.zburu.com/list/jquery3.6.0.js
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  $(function(){
+    console.log('tampermonkey')
+  })
+})()
